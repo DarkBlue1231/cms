4606e676b1f3946740fcd52d9bee11cdda3445c4
DarkBlue1231 <dekiing2@web.de>
2018-07-06 12:55:37 +0200

    Delete web.php

--- web.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-Route::get('dashboard/settings', 'Admin\SettingsController@index');
-Route::put('dashboard/settings', ['as' => 'dashboard.settings.update', 'uses' => 'Admin\SettingsController@update']);
